affed24c855738d5058c8aadce26c5bc9bb0d9e4
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-06-26 15:15:32 +0200

    Implemented Map Based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/pl/iamkonradkrakowiecki/sfgpetclinic/services/map/VetMapService.java
@@ -0,0 +1,35 @@
+package pl.iamkonradkrakowiecki.sfgpetclinic.services.map;
+
+import pl.iamkonradkrakowiecki.sfgpetclinic.model.Vet;
+import pl.iamkonradkrakowiecki.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
+
