e3aafd86f32b9ee1fe511e0143af075260708767
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-06-26 09:54:38 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/pl/iamkonradkrakowiecki/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package pl.iamkonradkrakowiecki.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/pl/iamkonradkrakowiecki/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package pl.iamkonradkrakowiecki.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
