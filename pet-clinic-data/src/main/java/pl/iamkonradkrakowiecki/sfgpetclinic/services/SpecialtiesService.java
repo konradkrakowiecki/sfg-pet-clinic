adb3f31b5ca51e5bb85f7387e2be899f58cd2686
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-07-01 11:46:14 +0200

    Adding Specialties Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/pl/iamkonradkrakowiecki/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package pl.iamkonradkrakowiecki.sfgpetclinic.services;
+
+import pl.iamkonradkrakowiecki.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
