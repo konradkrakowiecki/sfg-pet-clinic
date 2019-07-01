e3ae109341c8a478379fd53865f7b45d7ad39064
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-07-01 10:54:45 +0200

    Adding PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/pl/iamkonradkrakowiecki/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package pl.iamkonradkrakowiecki.sfgpetclinic.services;
+
+import pl.iamkonradkrakowiecki.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
