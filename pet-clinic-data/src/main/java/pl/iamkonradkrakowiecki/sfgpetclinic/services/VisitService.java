e62c1e58c76702e36a74a76f723840f7cbac1f12
konradkrakowiecki <konrad.krakowiecki@gmail.com>
2019-07-02 18:43:02 +0200

    Implemented Map Impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/pl/iamkonradkrakowiecki/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package pl.iamkonradkrakowiecki.sfgpetclinic.services;
+
+import pl.iamkonradkrakowiecki.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+
+}
